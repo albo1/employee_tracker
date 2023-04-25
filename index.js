13fcd7bbfba94f0ab4890ddc6a2104c7e88aa006
alboy77 <al.boucetta@yahoo.com>
2023-04-24 23:57:24 -0400

    connection to sql made

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 cTable;
 
+const db = mysql.createConnection(
+    {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'ALLmight',
+      database: 'employees_db'
+    },
+    console.log(`Connected to Employee Database!`)
+  );
