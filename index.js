9939e90b1ce067b9e3c9986819b0ecbc30e328f6
alboy77 <al.boucetta@yahoo.com>
2023-04-25 17:00:23 -0400

    nothing seems to be working

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function initQuest() {
     })
     .then(answer => {
       console.log('answer', answer);
-      switch (answer.option) {
+      switch (answer.options) {
         case promptMessages.viewDepartments:
           viewDepartments();
           break;
@@ -68,6 +68,7 @@ function initQuest() {
           break;
         case promptMessages.quit:
           db.end();
+          initQuest();
           break;
       }
 
@@ -76,6 +77,28 @@ function initQuest() {
 
 function viewDepartments() {
   const query = 'SELECT name AS Department, id AS "Department ID" FROM department';
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+    initQuest();
+  });
+}
+
+function viewRoles() {
+  const query = 'SELECT role.title AS "Job Title", role.id AS "Role Id", department.name AS Department, role.salary AS Salary FROM role INNER JOIN department ON department.id = role.department_id';
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.table(result);
+    initQuest();
+  });
+}
+
+function viewEmployees() {
+  const query = 'SELECT e.id AS "Employee Id", e.first_name AS "First Name", e.last_name AS "Last Name", role.title AS "Job Title", department.name AS Department, role.salary AS Salary, m.first_name AS "Manager First Name", m.last_name AS "Manager Last Name" FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY e.id';
   db.query(query, (err, result) => {
     if (err) {
       console.log(err);
