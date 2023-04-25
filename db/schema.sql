fcb39622882e87bbfeaa7f859d7bd6fdac60afdf
alboy77 <al.boucetta@yahoo.com>
2023-04-25 00:20:28 -0400

    tables better

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,24 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
-CREATE TABLE department ();
-CREATE TABLE role ();
-CREATE TABLE employee ();
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+
+);
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    
+);
 
 
